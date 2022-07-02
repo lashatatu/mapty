483e5383875aeab3e3f5b1429e6bb9175e05b624
lasha tatulashvili <lashatatu@gmail.com>
2022-07-02 23:25:00 +0400

    map marker

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 'use strict';
 
 // prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
 
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
@@ -11,15 +24,33 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-if(navigator.geolocation){
-  navigator.geolocation.getCurrentPosition(function(position){
-    const { latitude }=position.coords
-    const { longitude }=position.coords
+if ( navigator.geolocation ) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
     console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-  },function(){
-    alert('could not get your position')
-  })
+    const coords=[latitude,longitude]
+
+    // leaflet code start
+    const map = L.map('map')
+      .setView(coords, 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
+    })
+      .addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
+
+    // leaflet code end
+
+  }, function() {
+    alert('could not get your position');
+  });
 }
 
 
